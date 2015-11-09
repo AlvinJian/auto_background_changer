88c0de1fca151f9447ce7e40e5ae5052d2b1abc5
alvin_jian <alvinchien0624@gmail.com>
2015-11-09 18:37:23 +0800

    add custom scripts as backends

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-from setuptools import setup, find_packages
-from os import path
+try:
+    from setuptools import setup, find_packages, Extension
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages, Extension
+
+data_files=[
+    ('/etc/autobgch/scripts/',['autobgch/scripts/feh']),
+    ('/etc/autobgch/scripts/',['autobgch/scripts/mate']),
+    ('/etc/autobgch/scripts/',['autobgch/scripts/gnome3']),
+    ('/etc/autobgch/scripts/',['autobgch/scripts/unity']),
+]
 
 setup(
     name='auto background changer',
-    version='0.2.1',
+    version='0.3',
     description='A simple wallpaper changer',
-    long_description='A simple wallpaper changer based on feh for Linux(X11)',
+    long_description="""A simple wallpaper changer supporting multiple backends
+        for Linux or other X11 system""",
     url='https://github.com/AlvinJian/auto_background_changer',
     author='AlvinJian',
     author_email='alvinchien0624@gmail.com',
     keywords='wallpaper changer',
     packages=find_packages(),
-    dependency_links = ['http://feh.finalrewind.org/'],
+    package_data = {'autobgch':['scripts/*']},
+    data_files=data_files,
     entry_points={
         'console_scripts': [
             'bgchd=autobgch.bgchd:run',
