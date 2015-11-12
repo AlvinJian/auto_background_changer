40d71ffd7c58ff9cec479fa05674128e3fbc3a8c
alvin_jian <alvinchien0624@gmail.com>
2015-11-12 18:16:24 +0800

    correct descriptions

--- autobgch/bgchd.py
+++ autobgch/bgchd.py
@@ -12,7 +12,8 @@ def run():
     parser = argparse.ArgumentParser(description='random wallpaper changer daemon')
     parser.add_argument('-dir', dest='bg_dir', type=str, required=True, help='wallpaper directory')
     parser.add_argument('-intv', dest='intv', type=str, default='30s', metavar='MIN_OR_SEC', help='interval of changing wallpaper(i.e. 10s or 5m). default: 30s')
-    parser.add_argument('-bcknd', dest='bcknd', type=str, required=True, metavar='SCRIPT', help='script in /etc/autobgch/scripts/ as backend')
+    parser.add_argument('-bcknd', dest='bcknd', type=str, required=True, metavar='SCRIPT', \
+        help='script in /etc/autobgch/scripts/ as backend. official support: mate, gnome3, unity, feh')
     parser.add_argument('-dbginfo', dest='dbginfo', action='store_true', help='enable extra info for debug')
     args = parser.parse_args()
 
